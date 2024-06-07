2eb6e3ebd339184f17954fd9268f831b08b84949
Tomas Thelander <tomas.thelander0@gmail.com>
2024-06-07 17:03:20 +0200

    ädetails cont

--- static/main.js
+++ static/main.js
@@ -1,32 +1,50 @@
-async function getData() {
-    const offset = (new Date()).getTimezoneOffset() * -1;
-    const response = await fetch('/now?offset=' + offset);
-    const data = await response.json();
+const apiClient = (
+    () => {
+        async function getNow() {
+            const offset = (new Date()).getTimezoneOffset() * -1;
+            const response = await fetch('/now?offset=' + offset);
+            const data = await response.json();
 
-    return {
-        ...parseDate(data._embedded['repcal:date']),
-        ...parseTime(data._embedded['repcal:time'])
-    };
-}
+            return {
+                ...parseDate(data._embedded['repcal:date']),
+                ...parseTime(data._embedded['repcal:time'])
+            };
+        }
 
-function parseDate(api_date) {
-    const links = api_date._links['repcal:meta-day'];
-    const uiLink = links.find(l => l.name === 'ui');
+        async function convertDate(dateString) {
+            const [year, month, date] = dateString.split('-');
+            const response = await fetch(`/date/${year}/${month}/${date}`);
+            const data = await response.json();
+            return parseDate(data);
+        }
 
-    return {
-        date: api_date.texts.default,
-        celebrating: uiLink.title,
-        wiki: uiLink.href
+        function parseDate(api_date) {
+            const uiLink = api_date
+                ._links['repcal:meta-day']
+                .find(l => l.name === 'ui');
+
+            return {
+                date: api_date.texts.default,
+                celebrating: uiLink.title,
+                wiki: uiLink.href
+            }
+        }
+
+        function parseTime(api_time) {
+            return {
+                hour: api_time.attributes.hour,
+                minute: api_time.attributes.minute,
+                second: api_time.attributes.second
+            };
+        }
+
+        return {
+            getNow,
+            convertDate
+        };
     }
-}
+)();
 
-function parseTime(api_time) {
-    return {
-        hour: api_time.attributes.hour,
-        minute: api_time.attributes.minute,
-        second: api_time.attributes.second
-    };
-}
 
 const nowApp = () => ({
     date: 'Loading...',
@@ -51,7 +69,7 @@ const nowApp = () => ({
     },
 
     async sync() {
-        Object.assign(this, await getData());
+        Object.assign(this, await apiClient.getNow());
     },
 
     tick() {
@@ -68,5 +86,20 @@ const nowApp = () => ({
                 }
             }
         }
+    }
+});
+
+const dateDetailsApp = () => ({
+    input: '',
+
+    init() {
+        const now = new Date();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0');
+        const date = now.getDate().toString().padStart(2, '0');
+        this.input = `${now.getFullYear()}-${month}-${date}`;
+    },
+
+    convert() {
+        apiClient.convertDate(this.input);
     }
 });
